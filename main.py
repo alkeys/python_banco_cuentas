ac7d8ce9f697736bf94b39127299ddf2dc43ffdc
alexa <am18007@ues.edu.sv>
2021-11-02 22:18:48 -0600

    version 2.0 metodos agregados

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+#se cargar las cuentas de los archivos locales
 def cargar_datos():
       cont=0
       n=0
@@ -6,23 +6,40 @@ def cargar_datos():
       matrix=crearMatrisString(CantCuentas(),4)
       while(verda):
           try:
-              aux=""
-              aux+="cuentas\cuenta_"+str(cont)+".txt"
-              miFile=open(aux,'r+')
+              File=""
+              File+="cuentas\cuenta_"+str(cont)+".txt"
+              miFile=open(File,'r+')
               matrix[n][0] = miFile.readline()
-              matrix[n][0 ]= matrix[n][0][0:len(matrix[n][0])-1]
+              matrix[n][0] = matrix[n][0][0:len(matrix[n][0])-1]
               matrix[n][1] = miFile.readline()
-              matrix[n][1] =matrix[n][1][0:len(matrix[n][1])-1]
+              matrix[n][1] = matrix[n][1][0:len(matrix[n][1])-1]
               matrix[n][2] = miFile.readline()
               matrix[n][2] = matrix[n][2][0:len(matrix[n][2])-1]
               matrix[n][3] = miFile.readline()
-              matrix[n][3] = matrix[n][3][0:len(matrix[n][3])-1]
           except FileNotFoundError:
             verda=False
           cont+=1
           n+=1
       return matrix
 
+def GuardarDatos():
+    global numeroDeCuenta
+    global matrixCuentas
+    pos=buscarPoscuenta(numeroDeCuenta)
+    File = ""
+    File += "cuentas\cuenta_"+str(pos)+".txt"
+    try:
+        GuardarDatosFile=open(File , "w")
+        auxDatosCuenta = matrixCuentas[pos][0] + "\n"
+        auxDatosCuenta += matrixCuentas[pos][1] + "\n"
+        auxDatosCuenta += matrixCuentas[pos][2] + "\n"
+        auxDatosCuenta += matrixCuentas[pos][3]
+        GuardarDatosFile.write(auxDatosCuenta)
+        GuardarDatosFile.close()
+    except FileNotFoundError:
+        return False
+
+#devuelve la cantidad de cuentas
 def CantCuentas():
     verda=True
     cont=0
@@ -43,17 +60,58 @@ def crearMatrisString(f,c):
         matrix.append(MatrixAux)
     return matrix
 
+#true si existe y false si no
 def verificarCuenta(numeroCuenta):
     global matrixCuentas
-    global CantCuentasN
+    CantCuentasN=CantCuentas()
     for i in range(CantCuentasN):
         if numeroCuenta == matrixCuentas[i][1]:
             return True
+    return False
+
+#busca la posicion de la cuenta i retorna la pos y si no existe retorna -1
+def buscarPoscuenta(numeroCuenta):
+    global matrixCuentas
+    numeroCuentafinal=0
+    for i in range(CantCuentas()):
+        if numeroCuenta == matrixCuentas[i][1]:
+            return i
+    return -1
+
+#si es correcta de devuelve un true si no un false
+def verificarPassword(contra):
+    global numeroDeCuenta
+    global matrixCuentas
+    pos=buscarPoscuenta(numeroDeCuenta)
+    if pos!=-1:
+        print(matrixCuentas[pos][3])
+        if matrixCuentas[pos][3] == contra:
+            return True
+    return False
+
+#devuelve un True si esta correcto y un False si no hay fondos o esta mal el pass o la cuenta
+def retirarDinero(cantDinero):
+    global numeroDeCuenta
+    global passDeCuenta
+    global matrixCuentas
+    dineroFloat=0
+    if verificarCuenta(numeroDeCuenta):
+        if verificarPassword(passDeCuenta):
+            dineroFloat=float(matrixCuentas[buscarPoscuenta(numeroDeCuenta)][2])
+            if dineroFloat>cantDinero:
+                dineroFloat=dineroFloat-cantDinero
+                matrixCuentas[buscarPoscuenta(numeroDeCuenta)][2]=str(dineroFloat)
+                return True
+            else:
+                print("no hay suficientes fondos")
+                return False
+    return False
 
 
-
-
-
-#main paguina principal de variables globales etc
+#variables globales y temporales etc
 matrixCuentas=cargar_datos()
-CantCuentasN=CantCuentas()
+#flotantes
+numeroDeCuenta="12345"
+passDeCuenta="1234"
+
+
